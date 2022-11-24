2abb70a7a46515b0e54c54f0c43466a913e70161
DudyShavit <shavit.dudy@gmail.com>
2022-11-24 10:50:22 +0200

    Create add_new_dir_to_path.ps1

---
+++ add_new_dir_to_path.ps1
@@ -0,0 +1,13 @@
+Function Add-PathVariable {
+    param (
+        [string]$addPath
+    )
+    if (Test-Path $addPath){
+        $regexAddPath = [regex]::Escape($addPath)
+        $arrPath = $env:Path -split ';' | Where-Object {$_ -notMatch 
+"^$regexAddPath\\?"}
+        $env:Path = ($arrPath + $addPath) -join ';'
+    } else {
+        Throw "'$addPath' is not a valid path."
+    }
+}
